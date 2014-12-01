6df6d468feba5cad05eca18c7d9c7541d791452a
zjhiphop <zjhiphop@gmail.com>
2014-12-01 14:54:12 +0800

    add python test demo

---
+++ test/deflate.py
@@ -0,0 +1,11 @@
+import zlib
+import base64
+
+def decode_base64_and_inflate( b64string ):
+    decoded_data = base64.b64decode( b64string )
+    return zlib.decompress( decoded_data , -15)
+
+def deflate_and_base64_encode( string_val ):
+    zlibbed_str = zlib.compress( string_val )
+    compressed_string = zlibbed_str[2:-4]
+    return base64.b64encode( compressed_string )
